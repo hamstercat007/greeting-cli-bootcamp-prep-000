1b84f41af210f33f998b68321e2916e2e0db68de
Lisa Mac <lisamac421@gmail.com>
2018-08-29 18:34:34 +0000

    automatically backed up by learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,5 +1,7 @@
 # code the #greeting method here!
 #in the lib - you have all the logic, functions, classes everything
+#can be imported elsewhere
+
 def greeting(name) 
   print "Hello #{name}. It's nice to meet you."
 end 
